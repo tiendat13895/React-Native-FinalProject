2ec57f62c437e1e93e01780e00a13e7c70cdce2e
dat13895 <tiendat13895@gmail.com>
2023-09-25 20:45:03 +0700

    Complete Project

--- FinalProject/Home/Home.tsx
+++ FinalProject/Home/Home.tsx
@@ -8,18 +8,27 @@ import {
   FlatList,
   ListRenderItemInfo,
   Pressable,
+  SafeAreaView,
+  StatusBar,
+  Alert,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Carousel from 'react-native-snap-carousel';
-import CarouselCardItem, {SLIDER_WIDTH, ITEM_WIDTH} from './CarouselCardItem';
+// import Carousel from 'react-native-snap-carousel';
+// import CarouselCardItem, {SLIDER_WIDTH, ITEM_WIDTH} from './CarouselCardItem';
 import data from './data';
 import {Result, RootObject} from '../Product/model';
 import axios from 'axios';
 
 export function HomeScreen({navigation}: any) {
   const [name, setName] = useState('');
-  const isCarousel = React.useRef(null);
+  // const isCarousel = React.useRef(null);
   const [productData, setProductData] = useState<RootObject | undefined>();
+
+  const images = [
+    'https://images2.thanhnien.vn/Uploaded/anhdh/2022_03_08/anh-nhan-vat-cung-cap-8199.jpg',
+    'https://images2.thanhnien.vn/zoom/328_205/Uploaded/dieutrangqc/2022_12_10/4-2221.png',
+    'https://images2.thanhnien.vn/zoom/736_460/528068263637045248/2023/8/19/cap-binh-bong-gom-binh-duong-tu-thoi-ma-chong-toi-de-lai-16924516013891022970621-0-0-1250-2000-crop-16924516183231425573353.jpg',
+  ];
 
   useEffect(() => {
     getProductFromApi().then(response => setProductData(response));
@@ -48,7 +57,7 @@ export function HomeScreen({navigation}: any) {
   const getProductFromApi = () => {
     return axios
       .get(
-        `https://650468e7c8869921ae24ff64.mockapi.io/api/product?completed=false&page=1&limit=5`,
+        `https://650468e7c8869921ae24ff64.mockapi.io/api/product?completed=false&page=1&limit=10&orderBy=unitPrice&order=desc`,
         {
           headers: {
             Accept: 'application/json',
@@ -60,7 +69,7 @@ export function HomeScreen({navigation}: any) {
           let Result = {
             page: 1,
             results: res.data,
-            total_pages: 20,
+            total_pages: 10,
             total_results: 100,
           };
           return Result;
@@ -74,48 +83,68 @@ export function HomeScreen({navigation}: any) {
   };
 
   return (
-    <View style={{paddingTop: 10}}>
-      <View
-        style={{
-          width: '100%',
-          backgroundColor: '#6495ED',
-          marginBottom: 10,
-        }}>
-        <Text style={styles.title}>{name}</Text>
-      </View>
-      <View
-        style={{
-          alignItems: 'center',
-          marginBottom: 10,
-        }}>
-        <Carousel
-          layout="tinder"
-          layoutCardOffset={9}
-          ref={isCarousel}
-          data={data}
-          renderItem={CarouselCardItem}
-          sliderWidth={SLIDER_WIDTH}
-          itemWidth={ITEM_WIDTH}
-          inactiveSlideShift={0}
-          useScrollView={true}
-        />
-      </View>
+    <SafeAreaView>
+      <StatusBar />
+      <View style={{paddingTop: 10}}>
+        <View
+          style={{
+            width: '100%',
+            backgroundColor: '#6495ED',
+            marginBottom: 10,
+          }}>
+          <Text style={styles.title}>{name}</Text>
+        </View>
+        <View style={{alignItems: 'center'}}>
+          {/* <Carousel
+            layout="tinder"
+            layoutCardOffset={9}
+            ref={isCarousel}
+            data={data}
+            renderItem={CarouselCardItem}
+            sliderWidth={SLIDER_WIDTH}
+            itemWidth={ITEM_WIDTH}
+            inactiveSlideShift={0}
+            useScrollView={true}
+          /> */}
 
-      <Text
-        style={{
-          borderTopColor: 'black',
-          borderTopWidth: 2,
-          width: 330,
-          margin: 10,
-          paddingTop: 10,
-          fontSize: 18,
-          color: '#6495ED',
-          fontWeight: 'bold',
-        }}>
-        Sản phẩm nổi bật
-      </Text>
-      <ScrollView>
-        <View style={{marginBottom: 200}}>
+          <FlatList
+            contentInset={{top: 0, bottom: 0, left: 0, right: 0}}
+            contentInsetAdjustmentBehavior="automatic"
+            data={data}
+            horizontal
+            pagingEnabled
+            snapToAlignment="center"
+            showsHorizontalScrollIndicator={false}
+            renderItem={({item}: ListRenderItemInfo<any>) => (
+              <View style={styles.bannerContainer}>
+                <Image style={styles.bannerimage} source={{uri: item.imgUrl}} />
+                <Text style={styles.header} numberOfLines={3}>
+                  {item.title}
+                </Text>
+                <Text style={styles.body} numberOfLines={10}>
+                  {item.body}
+                </Text>
+              </View>
+            )}
+            keyExtractor={(item: any, index: number) => `${item.id}-${index}`}
+          />
+        </View>
+
+        <Text
+          style={{
+            borderTopColor: 'black',
+            borderTopWidth: 2,
+            width: 330,
+            margin: 10,
+            paddingTop: 10,
+            fontSize: 18,
+            color: '#6495ED',
+            fontWeight: 'bold',
+          }}>
+          Sản phẩm nổi bật
+        </Text>
+
+        <View style={{marginBottom: 20}}>
           {productData === undefined ? (
             <Text>Loading ...</Text>
           ) : (
@@ -124,6 +153,10 @@ export function HomeScreen({navigation}: any) {
               contentInset={{top: 0, bottom: 80, left: 0, right: 0}}
               contentInsetAdjustmentBehavior="automatic"
               data={productData?.results}
+              horizontal
+              pagingEnabled
+              snapToAlignment="center"
+              showsHorizontalScrollIndicator={false}
               renderItem={({item}: ListRenderItemInfo<Result>) => (
                 <Pressable
                   style={styles.rootItem}
@@ -161,14 +194,12 @@ export function HomeScreen({navigation}: any) {
                     <Text style={styles.itemCategoryText} numberOfLines={1}>
                       {item.categoryName}
                     </Text>
-                    <View style={styles.rootItem}>
-                      <Text style={styles.itemPriceText} numberOfLines={1}>
-                        {item.unitPrice.toLocaleString(undefined, {
-                          maximumFractionDigits: 2,
-                        })}
-                        đ
-                      </Text>
-                    </View>
+                    <Text style={styles.itemPriceText} numberOfLines={1}>
+                      {item.unitPrice.toLocaleString(undefined, {
+                        maximumFractionDigits: 2,
+                      })}
+                      đ
+                    </Text>
                   </View>
                 </Pressable>
               )}
@@ -178,20 +209,12 @@ export function HomeScreen({navigation}: any) {
             />
           )}
         </View>
-      </ScrollView>
-    </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
-    width: '90%',
-    borderRadius: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 40,
-    marginVertical: 10,
-  },
   title: {
     color: 'white',
     fontSize: 15,
@@ -202,6 +225,7 @@ const styles = StyleSheet.create({
   rootItem: {
     flexDirection: 'row',
     marginVertical: 5,
+    width: 350,
   },
   itemTitleText: {
     fontSize: 15,
@@ -214,18 +238,41 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   itemPriceText: {
-    fontSize: 20,
+    fontSize: 16,
     color: '#DAA520',
   },
   rightItem: {
-    flexDirection: 'column',
     marginHorizontal: 10,
-    flex: 1,
+    width: 200,
   },
   image: {
     width: 120,
     height: 120,
     marginLeft: 10,
     borderRadius: 20,
+  },
+  bannerimage: {
+    width: 250,
+    height: 200,
+    borderRadius: 20,
+  },
+  header: {
+    color: '#222',
+    fontSize: 16,
+    fontWeight: 'bold',
+    paddingTop: 10,
+    width: 300,
+    textAlign: 'center',
+  },
+  body: {
+    color: '#222',
+    fontSize: 13,
+    width: 330,
+    paddingLeft: 10,
+  },
+  bannerContainer: {
+    width: 350,
+    height: 380,
+    alignItems: 'center',
   },
 });
